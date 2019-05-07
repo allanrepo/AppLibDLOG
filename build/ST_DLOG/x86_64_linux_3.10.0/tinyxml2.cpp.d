07eefbbf0e6c42053d44a0d56f9a23234054ba0c
localuser <allan.asis@gmail.com>
2019-05-07 10:58:05 +0800

    snapshot of working version used in ST TPY trial with U1709 component 8

---
+++ build/ST_DLOG/x86_64_linux_3.10.0/tinyxml2.cpp.d
@@ -0,0 +1,5 @@
+LIBRARIES/DATALOG/build/ST_DLOG/x86_64_linux_3.10.0/tinyxml2.cpp.pre: \
+ LIBRARIES/DATALOG/xtrf/tinyxml2.cpp \
+ /export/home/prod/allan/ST/prog/UAI0_R10P18U1709/LIBRARIES/DATALOG/xtrf/tinyxml2.h
+
+/export/home/prod/allan/ST/prog/UAI0_R10P18U1709/LIBRARIES/DATALOG/xtrf/tinyxml2.h:
---
+++ build/ST_DLOG/x86_64_linux_3.10.0/xtrf.cpp.d
@@ -0,0 +1,8 @@
+LIBRARIES/DATALOG/build/ST_DLOG/x86_64_linux_3.10.0/xtrf.cpp.pre: \
+ LIBRARIES/DATALOG/xtrf/xtrf.cpp \
+ /export/home/prod/allan/ST/prog/UAI0_R10P18U1709/LIBRARIES/DATALOG/xtrf/xtrf.h \
+ /export/home/prod/allan/ST/prog/UAI0_R10P18U1709/LIBRARIES/DATALOG/xtrf/tinyxml2.h
+
+/export/home/prod/allan/ST/prog/UAI0_R10P18U1709/LIBRARIES/DATALOG/xtrf/xtrf.h:
+
+/export/home/prod/allan/ST/prog/UAI0_R10P18U1709/LIBRARIES/DATALOG/xtrf/tinyxml2.h:
